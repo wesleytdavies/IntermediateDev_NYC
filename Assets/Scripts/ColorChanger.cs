ea1ca744eec13e95b959b5276c0154648e4bee75
Wesley Davies <wtd224@nyu.edu>
2020-09-15 07:52:59 -0700

    Added all current project files - basic sprite animation

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    public Color charColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = charColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(0.02f, 0, 0));
+            GetComponent<Animator>().Play("walk_right");
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-0.02f, 0, 0));
+            GetComponent<Animator>().Play("walk_left");
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 0.02f, 0));
+            GetComponent<Animator>().Play("walk_up");
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -0.02f, 0));
+            GetComponent<Animator>().Play("walk_down");
+        }
+        else
+        {
+            GetComponent<Animator>().Play("idle");
+        }
+    }
+}
