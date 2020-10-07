acf13605f022d7e2edb569188b3da27a0894b70c
Wesley Davies <wtd224@nyu.edu>
2020-10-07 12:08:51 -0700

    added interactivity

---
+++ Assets/Scripts/Depth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<SpriteRenderer>().sortingOrder = Mathf.RoundToInt(transform.position.y);
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private int time = 0;
+    private int frameTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameTime++;
+        if(frameTime >= 60)
+        {
+            time++;
+            frameTime = 0;
+        }
+        GetComponent<TextMeshProUGUI>().text = "Time: " + time.ToString();
+    }
+}
